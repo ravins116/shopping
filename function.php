4fa17273334a62856fdc702ced3f577cfbd3dd0d
Arturs <>
2023-07-11 23:34:08 +0300

    #1 Task : Add refactoring data

--- function.php
+++ function.php
@@ -1,2 +1,30 @@
 <?php
- echo 'hello world';
+function ShoppingCost($order) {
+    $totalWeight = 0;
+    foreach ($order as $item) {
+      $totalWeight += $item['weight'] * $item['quantity'];
+    }
+    
+    if ($totalWeight < 1) {
+      $shippingCost = 5;
+      echo "<p>Your shipping cost is $shippingCost</p>";
+    } else if ($totalWeight < 5) {
+      $shippingCost = 10;
+      echo "<p>Your shipping cost is $shippingCost</p>";
+    } else if ($totalWeight < 20) {
+      $shippingCost = 20;
+      echo "<p>Your shipping cost is $shippingCost</p>";
+    } else {
+      $shippingCost = 50;
+      echo "<p>Your shipping cost is $shippingCost</p>";
+    }
+    
+    $orderTotal = 0;
+    foreach ($order as $item) {
+      $orderTotal += $item['price'] * $item['quantity'];
+    }
+    
+    echo "<p>Your order total is $orderTotal</p>";
+    echo "<p>Your total cost with shipping is " . ($orderTotal + $shippingCost) . "</p>";
+  }
+  
