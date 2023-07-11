cc000f324d9e3faf7d1d30bdc03d19116ee33dd7
Arturs <>
2023-07-12 01:44:17 +0300

    #2 Task: init

---
+++ app/ShoppingCart.php
@@ -0,0 +1,36 @@
+<?php class ShoppingCart {
+  private $items;
+
+  function __construct() {
+    $this->items = array();
+  }
+
+  function addItem($item) {
+    $this->items[$item['id']] = $item;
+  }
+
+  function removeItem($itemId) {
+    unset($this->items[$itemId]);
+  }
+
+  function calculateTotal() {
+    $total = 0;
+    foreach($this->items as $item) {
+      $total += $item['price'];
+    }
+    echo "Total: $total";
+  }
+
+  function checkout() {
+    $this->calculateTotal();
+    $this->items = array();
+  }
+}
+
+$cart = new ShoppingCart();
+
+$cart->addItem(array('id' => 1, 'name' => 'Item 1', 'price' => 20.5));
+$cart->addItem(array('id' => 2, 'name' => 'Item 2', 'price' => 10.0));
+
+$cart->removeItem(3);
+$cart->checkout();
