b0b0aacf16fe8f5cf50416a4778c2778fb96fbb5
Arturs <>
2023-07-12 03:41:15 +0300

    #3 Task: Add unit tests for cart functions

--- readme.md
+++ readme.md
@@ -3,7 +3,8 @@
 2. Debug ShoppingCart code, fix found bugs and provide example report on fixed issue
 - Fixed removeItem() function.
 - Idea: after each product added, need recalculate total cart costs.
-
+3. Create a suite of unit tests for the existing ~~Shopping~~Cart class.
+4. [..]Not found
 
 ###Useful terminal commands
 - vendor/bin/phpunit tests
--- tests/ShoppingCartTest.php
+++ tests/ShoppingCartTest.php
@@ -5,6 +5,56 @@ use App\Shopping\Product;
 
 final class ShoppingCartTest extends TestCase
 {
+    public function testEmptyCart()
+    {
+        $cart = new Cart();
+        $items = $cart->items();
+        $this->assertEmpty($items);
+    }
+
+    public function testAddItem()
+    {
+        $cart = new Cart();
+        $item = new Product(1, 'Item 2', 10.0);
+        $cart->addItem($item);
+        $items = $cart->items();
+        $this->assertCount(1, $items);
+        $this->assertSame($item, $items[1]);
+    }
+
+    public function testRemoveItem()
+    {
+        $cart = new Cart();
+        $item = new Product(5, 'Item 1', 10.0);
+        $cart->addItem($item);
+        $cart->removeItem(5);
+        $items = $cart->items();
+        $this->assertEmpty($items);
+    }
+
+    public function testCalculateTotal()
+    {
+        $cart = new Cart();
+        $product1 = new Product(1, 'Product A', 10.0);
+        $product2 = new Product(2, 'Product B', 20.0);
+        $cart->addItem($product1);
+        $cart->addItem($product2);
+        $total = $cart->calculateTotal();
+        $this->assertEquals(30.0, $total);
+    }
+
+    public function testCheckout()
+    {
+        $cart = new Cart();
+        $item1 = new Product(1, 'Item 1', 20.5);
+        $item2 = new Product(2, 'Item 2', 10.0);
+        $cart->addItem($item1);
+        $cart->addItem($item2);
+        $checkoutResult = $cart->checkout();
+        $items = $cart->items();
+        $this->assertEmpty($items);
+        $this->assertEquals('Total: 30.5', $checkoutResult);
+    }
     public function testShippingCart()
     {
         $cart = new Cart();
